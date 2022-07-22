1b1c5a3a25851fe7e8299d0b82bb06dccf2c51d5
Pranav Shrikhande <pranav.shrikhande@gmail.com>
2022-07-22 14:47:18 -0400

    Add files via upload

---
+++ department-service/src/main/java/repository/DepartmentRepository.java
@@ -0,0 +1,13 @@
+package repository;
+
+import entity.Department;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DepartmentRepository extends JpaRepository<Department,Long> {
+
+
+
+    Department findByDepartmenId(Long departmentId);
+}
